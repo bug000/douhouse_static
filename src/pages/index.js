3a8695b92580d78d155f27c3ee31e67921314c61
hhxx2015 <zhenyuan_hao@163.com>
2018-10-30 23:18:12 +0800

    build

--- src/pages/index.js
+++ src/pages/index.js
@@ -193,32 +193,17 @@ class MapApp2 extends React.Component {
 }
 
 
-class HouseMessDiv extends React.Component{
-
-  constructor(){
-    super();
-  }
-  render(){
-    console.info(this.props);
-    return <div>
-
-      <p>{this.props.title}</p>
-      <a href={this.props.id}>{this.props.title}</a>
-    </div>
-  }
-
-}
-
 
 class MapApp extends React.Component{
   constructor(){
     super();
     // this.markers = randomMarker(1000);
     // this.markers = [];
-    this.center = {longitude: 121, latitude: 31};
+    this.center = {longitude: 121.44033, latitude: 31.18594};
     this.state = {
       houseShow: "",
       places: [],
+      open: true,
     };
 
     this.markersEvents = {
@@ -253,12 +238,77 @@ class MapApp extends React.Component{
     });
   };
 
+  onOpenChange = (...args) => {
+    console.log(args);
+    this.setState({ open: !this.state.open });
+  }
+
+
   render(){
+
+    const sidebar = (<List>
+      {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].map((i, index) => {
+        if (index === 0) {
+          return (<List.Item key={index}
+                             thumb="https://zos.alipayobjects.com/rmsportal/eOZidTabPoEbPeU.png"
+                             multipleLine
+          >Category</List.Item>);
+        }
+        return (<List.Item key={index}
+                           thumb="https://zos.alipayobjects.com/rmsportal/eOZidTabPoEbPeU.png"
+        >Category{index}</List.Item>);
+      })}
+    </List>);
+
+    this.amapEvents = {
+      created: (mapInstance) => {
+        // mapInstance.setMapStyle('amap://styles/cd53d7e5f1aab24ae9f75992aaf75285');
+        mapInstance.setMapStyle('amap://styles/darkblue');
+      }
+    };
+
     return <div>
       <PlaceList transferHouseMsg={this.getPlaceData}/>
+      <NavBar icon={<Icon type="ellipsis" />} onLeftClick={() => this.onDock('docked')}>
+
+        Docked in document
+      </NavBar>
+      <Drawer
+        className="my-drawer"
+        style={{ minHeight: document.documentElement.clientHeight }}
+        enableDragHandle
+        contentStyle={{ color: '#A6A6A6', textAlign: 'center', paddingTop: 42 }}
+        sidebar={sidebar}
+        open={this.state.open}
+        onOpenChange={this.onOpenChange}
+      >
+        Click upper-left corner
+      </Drawer>
       <div style={{width: '100%', height: 500}}>
-        <Map plugins={['ToolBar']} center={this.center} zoom={11}>
+        <Map plugins={
+          [
+            // 'ToolBar',
+            // 'MapType',
+            // 'Scale',
+            // 'OverView',
+            // 'ControlBar', // v1.1.0 新增
+            {
+              name: 'ToolBar',
+              options: {
+                visible: true,  // 不设置该属性默认就是 true
+                onCreated(ins){
+                  // console.log(ins);
+                },
+              },
+            }
+        ]}
+             events={this.amapEvents}
+             // mapStyle={"cd53d7e5f1aab24ae9f75992aaf75285"}
+             // mapStyle={"darkblue"}
+             amapkey={"398ead94587b740036e874a2ba6f4252"}
+             center={this.center} zoom={11}>
           <Markers
+
             markers={this.state.places}
             useCluster={true}
             events={this.markersEvents}
@@ -266,6 +316,7 @@ class MapApp extends React.Component{
           />
         </Map>
       </div>
+
       <div>
         {/*<p>{this.state.houseShow["title"]}</p>*/}
         <hr/>
